2dc8d74e069be56b5358b2e4f120073e7dbb05ba
pk635890 <pk635890@host.docker.internal>
2020-03-11 15:38:42 +0530

    Added timestamp in sample message

--- src/main/java/org/pd/streaming/connector/es/SampleMessage.java
+++ src/main/java/org/pd/streaming/connector/es/SampleMessage.java
@@ -1,5 +1,7 @@
 package org.pd.streaming.connector.es;
 
+import java.time.LocalDateTime;
+
 import lombok.Data;
 
 @Data
@@ -7,10 +9,12 @@ public class SampleMessage
 {
     String metric_name;
     Long metric_value;
+    String timestamp;
     
     public SampleMessage(String name, Long value)
     {
         this.metric_name = name;
         this.metric_value = value;
+        this.timestamp = LocalDateTime.now().toString();
     }
 }
